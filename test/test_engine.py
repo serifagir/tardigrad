94977c7d590dbe5f286314a05d554dcbd217b47c
serifagir <serifagir64@gmail.com>
2024-07-13 14:56:21 +0300

    test script

---
+++ test/test_engine.py
@@ -0,0 +1,67 @@
+import torch
+from tardigrad.engine import Scalar
+
+def test_overall_check():
+
+    x = Scalar(-4.0)
+    z = 2 * x + 2 + x
+    q = z.tanh() + z * x
+    h = (z * z).tanh()
+    y = h + q + q * x
+    y.backward_prop()
+    xmg, ymg = x, y
+
+    x = torch.Tensor([-4.0]).double()
+    x.requires_grad = True
+    z = 2 * x + 2 + x
+    q = z.tanh() + z * x
+    h = (z * z).tanh()
+    y = h + q + q * x
+    y.backward_prop()
+    xpt, ypt = x, y
+
+    # forward pass went well
+    assert ymg.data == ypt.data.item()
+    # backward pass went well
+    assert xmg.grad == xpt.grad.item()
+
+def test_extra_ops():
+
+    a = Scalar(-4.0)
+    b = Scalar(2.0)
+    c = a + b
+    d = a * b + b**3
+    c += c + 1
+    c += 1 + c + (-a)
+    d += d * 2 + (b + a).tanh()
+    d += 3 * d + (b - a).tanh()
+    e = c - d
+    f = e**2
+    g = f / 2.0
+    g += 10.0 / f
+    g.backward_prop()
+    amg, bmg, gmg = a, b, g
+
+    a = torch.Tensor([-4.0]).double()
+    b = torch.Tensor([2.0]).double()
+    a.requires_grad = True
+    b.requires_grad = True
+    c = a + b
+    d = a * b + b**3
+    c = c + c + 1
+    c = c + 1 + c + (-a)
+    d = d + d * 2 + (b + a).tanh()
+    d = d + 3 * d + (b - a).tanh()
+    e = c - d
+    f = e**2
+    g = f / 2.0
+    g = g + 10.0 / f
+    g.backward_prop()
+    apt, bpt, gpt = a, b, g
+
+    tol = 1e-6
+    # forward pass went well
+    assert abs(gmg.data - gpt.data.item()) < tol
+    # backward pass went well
+    assert abs(amg.grad - apt.grad.item()) < tol
+    assert abs(bmg.grad - bpt.grad.item()) < tol
